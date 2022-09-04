d782513a714604ee48bd3db98fbb3defa7665b95
Thien Quach <thien.quach@codeenginestudio.com>
2022-09-04 15:00:54 +0700

    Hibernate: HQL - Hibernate Query Language - Example Tutorial

---
+++ HQLExample/src/main/java/hibernate/model/Address.java
@@ -0,0 +1,32 @@
+package hibernate.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Address")
+public class Address {
+    @Id
+    @Column(name = "emp_id", unique = true, nullable = false)
+    @GeneratedValue(generator = "gen")
+    @GenericGenerator(name = "gen", strategy = "foreign", parameters = {@org.hibernate.annotations.Parameter(name = "property", value = "employee")})
+    private long id;
+
+    @Column(name = "address_line1")
+    private String addressLine1;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @Column(name = "city")
+    private String city;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Employee employee;
+}
---
+++ HQLExample/src/main/java/hibernate/model/Employee.java
@@ -0,0 +1,34 @@
+package hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+@Getter
+@Setter
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private long id;
+
+    @Column(name = "emp_name")
+    private String name;
+
+    @Column(name = "emp_salary")
+    private double salary;
+
+    @OneToOne(mappedBy = "employee")
+    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
+    private Address address;
+}
